410212c5186a70a8eaf7590838b0e52a52f73bef
vinod <vinod.raghuwanshi@simplelogic.in>
2018-02-27 15:26:28 +0530

    vinod 27_02_2018 n

---
+++ src/com/anchor/activities/SubDealer_Home.java
@@ -0,0 +1,14 @@
+package com.anchor.activities;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class SubDealer_Home extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub_dealer__home);
+    }
+
+}
